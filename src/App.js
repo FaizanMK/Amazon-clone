a3189443fec0876aa667c69f1bc6e3c98df9b726
Your Name-asfasasfasf <you@example.com>
2022-08-25 17:00:32 +0500

    NavBar component completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-          <Route path="/" element={<h1>HOME PAGE!!!</h1>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header /> <h1>HOME PAGE!!</h1>
+              </>
+            }
+          />
           <Route path="/Login" element={<h1>LOGIN PAGE!!!</h1>} />
           <Route path="/checkout" element={<h1>CHECK-OUT PAGE!!!</h1>} />
 
